e3be0ad8fb3c550638ae9792596f7934b4168ac9
mihai <stereo12@mail.ru>
2023-10-09 22:46:31 +0300

    cleanup in folders and files

--- ORACLE_DB_LINKS.sql
+++
@@ -1,4 +0,0 @@
--- This will create a link named remote_patients from servermanagement to serverpatients.
-CREATE DATABASE LINK remote_patients
-CONNECT TO system IDENTIFIED BY Master2023
-USING '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=8091))(CONNECT_DATA=(SID=patients)))';
--- views/STAFF_PATIENT.sql
+++ views/STAFF_PATIENT.sql
@@ -1,4 +1,4 @@
 CREATE OR REPLACE VIEW staff_patient AS
 SELECT s.*, p.name AS patient_name, p.age AS patient_age
-FROM Staff s, Patient@remote_db2 p
+FROM Staff s, SYS.PATIENT@patients p
 WHERE s.id = p.staff_id;
